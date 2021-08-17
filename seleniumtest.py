f2556cd1b54485db362f0db23ecca904b14963ce
Juhi <mahima061998@gmail.com>
2021-08-17 23:07:19 +0530

    test with scrappy, selenium and postgres

---
+++ seleniumtest.py
@@ -0,0 +1,52 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def wait_for(condition_function):
+  start_time = time.time()
+  while time.time() < start_time + 20:
+    if condition_function():
+      return True
+    else:
+      time.sleep(1)
+  raise Exception(
+   'Timeout waiting for '
+  )
+
+options = Options()
+options.headless = True
+options.add_argument("--window-size=1920,1200")
+
+DRIVER_PATH = './chromedriver'
+driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
+driver.get('https://agmarknet.gov.in/SearchCmmMkt.aspx?Tx_Commodity=78&Tx_State=0&Tx_District=0&Tx_Market=0&DateFrom'
+           '=01-Jan-2021&DateTo=10-Aug-2021&Fr_Date=01-Jan-2021&To_Date=10-Aug-2021&Tx_Trend=0&Tx_CommodityHead'
+           '=Tomato&Tx_StateHead=--Select--&Tx_DistrictHead=--Select--&Tx_MarketHead=--Select--')
+# print(driver.page_source)
+table = driver.find_element_by_class_name('tableagmark_new')
+last_row = table.find_elements_by_tag_name("tr")[50].find_element_by_tag_name("td").text
+print (last_row)
+table.find_elements_by_tag_name("tr")[52].find_element_by_tag_name("input").click()
+
+
+# time.sleep(10)
+
+def page_hasLoaded():
+    try:
+        table = driver.find_element_by_class_name('tableagmark_new')
+        curr = table.find_elements_by_tag_name("tr")[50].find_element_by_tag_name("td").text
+        if curr == last_row:
+            return False
+        else:
+            return True
+    except Exception:
+        return False
+
+
+wait_for(page_hasLoaded)
+
+# driver.find_element_by_xpath("//input[@onclick=\"javascript:__doPostBack('ctl00$cphBody$GridPriceData','Page$Next');return false;\"]").click()
+
+driver.save_screenshot('ss.png')
+driver.quit()
--- web-scraping.py
+++ web-scraping.py
@@ -1,57 +1,73 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests
-import pandas as pd
-import mysql.connector
+import psycopg2
 
 host = "localhost"
-username = "user"
-password = "pass"
+username = "postgres"
+password = "password"
+database = "agriculturedb"
+port = "5432"
 
-# mydb = mysql.connector.connect(host=host, username=username, password=password)
-#
-# mycursor = mydb.cursor()
-#
-# mycursor.execute("CREATE DATABASE mydatabase")
-#
-# mycursor.execute("SHOW DATABASES")
-#
-# for x in mycursor:
-#   print(x)
+conn = psycopg2.connect(host=host, port=port, dbname=database, user=username, password=password)
+cursor = conn.cursor()
 
 
-url = "https://agmarknet.gov.in/SearchCmmMkt.aspx?Tx_Commodity=78&Tx_State=0&Tx_District=0&Tx_Market=0&DateFrom=01" \
-      "-Jan-2021&DateTo=10-Aug-2021&Fr_Date=01-Jan-2021&To_Date=10-Aug-2021&Tx_Trend=0&Tx_CommodityHead=Tomato" \
-      "&Tx_StateHead=--Select--&Tx_DistrictHead=--Select--&Tx_MarketHead=--Select-- "
+def parse_data_from_website():
+    url = "https://agmarknet.gov.in/SearchCmmMkt.aspx?Tx_Commodity=78&Tx_State=0&Tx_District=0&Tx_Market=0&DateFrom=01" \
+          "-Jan-2021&DateTo=10-Aug-2021&Fr_Date=01-Jan-2021&To_Date=10-Aug-2021&Tx_Trend=0&Tx_CommodityHead=Tomato" \
+          "&Tx_StateHead=--Select--&Tx_DistrictHead=--Select--&Tx_MarketHead=--Select-- "
 
-page_content = requests.get(url).text
+    page_content = requests.get(url).text
 
-# soup object to collect the required data
-soup = BeautifulSoup(page_content, "lxml")
+    # soup object to collect the required data
+    soup = BeautifulSoup(page_content, "lxml")
 
-# extracting the table and table data from the site
-tomato_price_table = soup.find("table", {"class": "tableagmark_new"})
-tomato_price_table_data = tomato_price_table.find_all("tr")
-# tomato_price_table_data = tomato_price_table_data[1:]
+    # extracting the table and table data from the site
+    tomato_price_table = soup.find("table", {"class": "tableagmark_new"})
+    tomato_price_table_data = tomato_price_table.find_all("tr")
 
-# print(tomato_price_table_data)
+    # print(tomato_price_table_data)
+    table_data = []
+    for tr in tomato_price_table.find_all("tr")[1:]:  # ignoring the tr with style
+        t_row = []
+        # print("TR = ",tr.text)
+        for td in tr.find_all("td"):
+            # print("TD = ",td.text)
+            td_text = td.text.replace('\n', ' ').strip()
+            if len(td_text) != 0:
+                t_row.append(td_text)
+        if len(t_row) != 0:
+            # print(t_row)
+            table_data.append(t_row)
 
-# Extracting column headings
-col_headings = []
-for th in tomato_price_table_data[0].find_all("th"):
-    # remove any newlines and extra spaces from left and right
-    col_headings.append(th.b.text.replace('\n', ' ').strip())
+    return table_data
 
-print(col_headings)
 
-table_data = []
-for tr in tomato_price_table.find_all("tr")[1:]:  # find all tr's from table's tbody
-    t_row = []
-    # print("TR = ",tr.text)
-    for td in tr.find_all("td"):
-        # print("TD = ",td.text)
-        t_row.append(td.text.replace('\n', ' ').strip())
-    table_data.append(t_row)
+""""
+    # Extracting column headings
+    col_headings = []
+    for th in tomato_price_table_data[0].find_all("th"):
+        # remove any newlines and extra spaces from left and right
+        col_headings.append(th.b.text.replace('\n', ' ').strip())
 
-print(table_data)
+    print(col_headings)"""
+
+
+def insert(data_arr, conn):
+    # insert_query = "INSERT INTO test_table values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+    # cursor.execute(insert_query, data_arr[0])
+    # conn.commit()
+    print(len(data_arr))
+    for row in range(len(data_arr)):
+        insert_query = "INSERT INTO test_table(districtname , marketname, commodity, variety , grade, " \
+                       "minprice , maxprice , modalprice , pricedate ) values (%s, %s, %s, %s, %s, %s, " \
+                       "%s, %s, %s); "
+        cursor.execute(insert_query, data_arr[row][1:])
+    conn.commit()
+
+
+parsed_data = parse_data_from_website()
+print(parsed_data)
+insert(parsed_data, conn)
 
